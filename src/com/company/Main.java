532d88336610a66bf051c2f4af195cd35a3a2920
Alexandre Lefebvre <alexandre_lefebvre@outlook.com>
2017-12-04 14:36:41 +0100

    helloworld

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import com.sun.prism.shader.Solid_ImagePattern_Loader;
+
+import java.io.Console;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Helloworld");
+    }
+}
